5dfefd7e9988f97b7b6edb298357d730dede15fd
Aaron <36244912+Aaron-LS@users.noreply.github.com>
2018-04-02 15:26:54 -0400

    Update clientproduction.pp

--- manifests/clientproduction.pp
+++ manifests/clientproduction.pp
@@ -3,6 +3,6 @@ class role::clientproduction {
     include profile::base
     include profile::java::production
     include profile::mysql::production
-    include profile::tomcat::prod
+    include profile::tomcat::production
 
 }
