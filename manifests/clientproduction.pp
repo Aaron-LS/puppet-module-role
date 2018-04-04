9948a771dcf5dced5e11af03e542455a803b95f5
Aaron Butler <abutler@legal-suite.com>
2018-04-04 15:40:50 -0400

    Adding Apache config.

--- manifests/clientproduction.pp
+++ manifests/clientproduction.pp
@@ -3,6 +3,7 @@ class role::clientproduction {
     include profile::base
     include profile::java::production
     include profile::mysql::production
+    include profile::apache::production
     include profile::tomcat::production
 
 }
